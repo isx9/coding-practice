9116a847ca40742bbc5aa7fb0781f34584e96a2a
Isabella Di Lorenzi <148529277+isx9@users.noreply.github.com>
2026-09-09 00:02:40 +0200

    Update collatz_conjecture.py

--- fundamentals/exercism/collatz_conjecture.py
+++ fundamentals/exercism/collatz_conjecture.py
@@ -2,11 +2,11 @@ def steps(number):
     """Return the number of steps to reach 1 following the Collatz Conjecture rules."""
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
-    steps = 0
+    steps_counter = 0
     while number != 1:
         if number % 2 == 0:
             number = number // 2
         else:
             number = number * 3 + 1
-        steps += 1
-    return steps
+        steps_counter += 1
+    return steps_counter
